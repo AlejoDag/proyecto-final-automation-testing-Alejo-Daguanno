8fd85d2d7b2441179c016a9c56b2857daf5a0854
AlejoDag <74698137+AlejoDag@users.noreply.github.com>
2025-12-15 18:23:48 -0300

    Update test_ui.py

--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -1,11 +1,19 @@
 import pytest
 import json
+import os
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 
-# Cargar datos desde JSON
-with open('datos/test_data.json') as f:
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(os.path.dirname(current_dir))
+
+json_path = os.path.join(project_root, 'datos', 'test_data.json')
+
+with open(json_path) as f:
     test_data = json.load(f)
+
 
 class TestUI:
     
@@ -18,7 +26,7 @@ class TestUI:
         
         assert inventory_page.get_title() == "Products"
 
-    @pytest.mark.parametrize("datos", test_data)
+    @pytest.mark.parametrize("data", test_data)
     def test_login_data_driven(self, driver, data):
         login_page = LoginPage(driver)
         inventory_page = InventoryPage(driver)
@@ -47,9 +55,14 @@ class TestUI:
         
         driver.get("https://www.saucedemo.com/")
         login_page.login("standard_user", "secret_sauce")
+
         inventory_page.add_backpack_to_cart()
+
+        assert inventory_page.get_cart_count() == "1"
+
         inventory_page.go_to_cart()
-        
+
+        WebDriverWait(driver, 10).until(EC.url_contains("cart.html"))
         assert "cart.html" in driver.current_url
 
     def test_fallido_intencional(self, driver):
